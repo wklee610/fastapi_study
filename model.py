35148e80b806e58233df92aed3cb3ce9dbb9989f
wklee610 <wklee610@naver.com>
2023-09-24 00:22:54 +0900

    Create model.py

---
+++ model.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class click(BaseModel):
+    user_id: str
+    log_dt: str
+    # content_id: str
+    # data1: str
+    # data2: str
+    # data3: str
+    # data4: str
+
